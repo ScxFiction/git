6a3bdccf5acba58906f93e113b6dfc3f8f5ba166
ScxFiction <scxfiction@gmail.com>
2015-02-01 06:50:58 +0100

    Admin area dynamic integration
    Show posts in admin area :1234:
    Submit,Cancel,Delete

--- blog_1/admin/index.php
+++ blog_1/admin/index.php
@@ -1,4 +1,15 @@
 <?php include 'includes/header.php'; ?>
+<?php 
+// Create Dabase Object
+$db = new Database;
+//Create Query
+$query = "SELECT posts.*, categories.name FROM posts
+            INNER JOIN categories
+            ON posts.category = categories.id";
+
+// Run Query
+$posts = $db->select($query);   
+?>
 
 <table class="table table-striped">
     <tr>
@@ -7,26 +18,28 @@
         <th>Category</th>
         <th>Author</th>
         <th>Date</th>
-    </tr>
+    </tr>  
+<?php while($row = $posts->fetch_assoc()): ?>
     <tr>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td><?php echo $row['id']; ?></td>
+        <td><a href="edit_post.php?id=<?php echo $row['id']; ?>"><?php echo $row['title']; ?></a></td>
+        <td><?php echo $row['name']; ?></td>
+        <td><?php echo $row['author']; ?></td>
+        <td><?php echo formatDate($row['date']); ?></td>
     </tr> 
-</table>
+<?php endwhile; ?>  
+    </table>
 
-<table class="table table-striped">
-    <tr>
-        <th>Category ID#</th>
-        <th>Category Name</th>
-    </tr>
-    <tr>
-        <td></td>
-        <td></td>
-    </tr>
-</table>
+    <table class="table table-striped">
+        <tr>
+            <th>Category ID#</th>
+            <th>Category Name</th>
+        </tr>
+        <tr>
+            <td></td>
+            <td></td>
+        </tr>
+    </table>
 
 
 <?php include 'includes/footer.php'; ?>
